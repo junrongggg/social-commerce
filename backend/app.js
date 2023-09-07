8f47602c4638864fedce0fdda27b3a0566969d86
Mark Cheong <marktakaema@gmail.com>
2023-09-07 15:42:10 +0800

    backend setup

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv'); // Import dotenv directly for ES modules
+
+const { db } = require('./db/db'); //importing from db.js file to connect to mongoDB
+
+dotenv.config();
+const PORT = process.env.PORT || 5002 ; // Provide a default port if PORT is not defined in the environment
+
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+
+//generate a connection to the mongo server
+// mongoose.connect("");
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+/** Connecting to server */
+const server = async () => {
+    try {
+        db()
+        app.listen(PORT, () => {
+            console.log('Server is listening on port:', PORT);
+        });
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+};
+
+server();
