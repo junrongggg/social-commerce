06ad7a38468d97149a9e44a20ed285892222726b
Mark Cheong <marktakaema@gmail.com>
2023-09-08 00:15:17 +0800

    Squashed commit of the following:
    commit 285b9a6899f489998c938941c3c66a75d2eeebbc
    Author: Mark Cheong <marktakaema@gmail.com>
    Date:   Fri Sep 8 00:14:50 2023 +0800
    
        update 2
    
    commit a3d29f238e64d0466bc9a8af34249514aa19c8c5
    Author: Mark Cheong <marktakaema@gmail.com>
    Date:   Fri Sep 8 00:14:00 2023 +0800
    
        feed page update

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,7 +1,69 @@
 import React from 'react';
-import Navbar from "../components/Navbar.js";
 
-export function Feed() {
-	// return <Navbar></Navbar>;
-}
+const ProductCard = ({ product }) => {
+  return (
+    <a href={product.href} className="group">
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+        <img
+          src={product.imageSrc}
+          alt={product.imageAlt}
+          className="h-full w-full object-cover object-center group-hover:opacity-75"
+        />
+      </div>
+      <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+    </a>
+  );
+};
 
+export const Feed = () => {
+	const products = [
+		{
+		id: 1,
+		name: 'Earthen Bottle',
+		href: '#',
+		price: '$48',
+		imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
+		imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+		},
+		{
+		id: 2,
+		name: 'Nomad Tumbler',
+		href: '#',
+		price: '$35',
+		imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
+		imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
+		},
+		{
+		id: 3,
+		name: 'Focus Paper Refill',
+		href: '#',
+		price: '$89',
+		imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+		imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+		},
+		{
+		id: 4,
+		name: 'Machined Mechanical Pencil',
+		href: '#',
+		price: '$35',
+		imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+		imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+		},
+		// More products...
+	]
+
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8">
+        <h2 className="text-3xl font-extrabold text-gray-900">Products</h2>
+
+        <div className="mt-10 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
