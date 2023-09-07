66617c29ea3f93d0eb6026acb9b9864138864604
marcoos21 <marcusmui1999@gmail.com>
2023-09-08 00:47:32 +0800

    Product Draft

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+//import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./pages/Feed.js";
+import ProductPage from "./pages/Product.js";
+function App() {
+  return <Navbar></Navbar>, <ProductPage></ProductPage>;
+}
+
+export default App;
---
+++ frontend/src/pages/Product.js
@@ -0,0 +1,6 @@
+import Navbar from "./components/Navbar.js";
+import Product from "./components/Product.js";
+
+export default function ProductPage() {
+    return <Navbar></Navbar> ,<Product></Product>;
+  }
