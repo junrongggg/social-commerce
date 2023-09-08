e3b0e94d027fb165e0cfbf6922d27da47483fc40
junrongggg <junrongt4n@gmail.com>
2023-09-08 16:27:40 +0800

    .

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const ProductCard = ({ product }) => {
   return (
-    <a href={product.href} className="group">
+    <a href={product.href} className="group p-3">
       <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
         <img
           src={product.imageSrc}
           alt={product.imageAlt}
-          className="h-full w-full object-cover object-center group-hover:opacity-75"
+          className="object-center group-hover:opacity-75 w-32"
         />
       </div>
       <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
@@ -17,64 +17,72 @@ const ProductCard = ({ product }) => {
 };
 
 export const Feed = () => {
-	const products = [
-		{
-		id: 1,
-		name: 'Earthen Bottle',
-		href: '#',
-		price: '$48',
-		imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
-		imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
-		},
-		{
-		id: 2,
-		name: 'Nomad Tumbler',
-		href: '#',
-		price: '$35',
-		imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
-		imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
-		},
-		{
-		id: 3,
-		name: 'Focus Paper Refill',
-		href: '#',
-		price: '$89',
-		imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
-		imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
-		},
-		{
-		id: 4,
-		name: 'Machined Mechanical Pencil',
-		href: '#',
-		price: '$35',
-		imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
-		imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
-		},
-		// More products...
-	]
+  const products = [
+    {
+      id: 1,
+      name: "Earthen Bottle",
+      href: "#",
+      price: "$48",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
+      imageAlt:
+        "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
+    },
+    {
+      id: 2,
+      name: "Nomad Tumbler",
+      href: "#",
+      price: "$35",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
+      imageAlt:
+        "Olive drab green insulated bottle with flared screw lid and flat top.",
+    },
+    {
+      id: 3,
+      name: "Focus Paper Refill",
+      href: "#",
+      price: "$89",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
+      imageAlt:
+        "Person using a pen to cross a task off a productivity paper card.",
+    },
+    {
+      id: 4,
+      name: "Machined Mechanical Pencil",
+      href: "#",
+      price: "$35",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
+      imageAlt:
+        "Hand holding black machined steel mechanical pencil with brass tip and top.",
+    },
+    // More products...
+  ];
 
-	return (
-		<div className="bg-white flex flex-col md:flex-row">
-			{/* Left Section */}
-			<div className="w-full md:w-3/4">
-				<div className="mx-auto max-w-7xl py-20 px-20 sm:px-6 lg:px-8">
-					<h2 className="text-3xl font-extrabold text-gray-900">Products</h2>
-					<div className="mt-10 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-						{products.map((product) => (
-							<ProductCard key={product.id} product={product} />
-						))}
-					</div>
-				</div>
-			</div>
+  return (
+    <div className="bg-white flex flex-col md:flex-row">
+      {/* Left Section */}
+      <div className="w-full md:w-3/4">
+        <div className="mx-auto max-w-7xl py-20 px-20 sm:px-6 lg:px-8">
+          <div className="mt-10 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"></div>
+        </div>
+      </div>
 
-			{/* Right Section for Ads */}
-				<div className="w-full md:w-1/4 bg-gray-200">
-				{/* Your ad content goes here */}
-				<div className="p-4">
-					<h3 className="text-xl font-semibold">Advertisement</h3>
-					{/* Ad content */}
-				</div>
-			</div>
-		</div>
-	);
+      {/* Right Section for Ads */}
+      <div className="w-full md:w-1/4 bg-gray-200">
+        {/* Your ad content goes here */}
+        <div className="p-4">
+          <h3 className="text-3xl font-extrabold text-gray-900	">
+            Flash deals!
+          </h3>
+          {/* Ad content */}
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
