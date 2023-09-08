96483d9f2b908350730ff9a3c6b88380fb485eee
Mark Cheong <marktakaema@gmail.com>
2023-09-09 03:04:18 +0800

    layout change + addition of wishlist popup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,8 @@ import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar.js";
 import { Feed } from "./pages/Feed.js";
 import { ProductPage } from "./pages/product.js";
-import {ToastContainer, toast} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
-	const notify = () => toast("You have a new follower!")
 	return(
 		<div className="App">
 			<Router>
@@ -21,10 +19,6 @@ function App() {
 					<Route path="/product" element={<ProductPage />}/>
 				</Routes>
 			</Router>
-			<div>
-				<button onClick = {notify}>Follow!</button>
-				<ToastContainer/>
-			</div>
 		</div>
 	);
 }
--- frontend/src/pages/product.js
+++ frontend/src/pages/product.js
@@ -2,11 +2,15 @@ import '../App.css';
 import React, { useState } from 'react';
 import StarRating from '../starRating';
 
+import {ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const ProductPage = () => {
   const [liked, setLiked] = useState(false);
   const [wishlist, setWishlist] = useState([]);
   const [discussion, setDiscussion] = useState([]);
+
+  const notify = () => toast("You have a new follower!")
   
   const toggleLike = () => {
     setLiked(!liked);
@@ -29,43 +33,40 @@ export const ProductPage = () => {
         <h2 className="text-2xl font-semibold">Product Name</h2>
         <p className="text-gray-600">$99.99</p>
         <p className="my-4">Product description goes here...</p>
-        <div className="w-1/3 p-4"><StarRating/></div>
-        <div className="w-2/3 p-4">
-        <button
-          onClick={toggleLike}
-          className={`flex items-center space-x-2 ${
-            liked ? 'text-red-500' : 'text-gray-500'
-          }`}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-          <span>{liked ? 'Liked' : 'Like'}</span>
-        </button>
+          
+        <div className="w-max p-2 pr-96">
+          <div className="flex items-center space-x-2">
+            {/* Star Ratings */}
+            <StarRating/>
+            <button
+              onClick={toggleLike}
+              className={`flex items-end space-x-2 ${ liked ? 'text-red-500' : 'text-gray-500'}`}
+            >
+              {/* Heart Shape Icon */}
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+              </svg>
+            </button>
+            {/* Share Icon */}
+            <button onClick={shareProduct} className="flex items-end text-gray-500 hover:text-gray-700">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
+              </svg>
+            </button>
+          </div>  
+        </div>
+        
+        <div className="w-1/3 p-2">
+				    <button onClick = {notify}>Add to Wishlist</button>
+            <ToastContainer/>
+            {/* <button
+              onClick={addToWishlist}
+              className="text-gray-500 hover:text-gray-700"
+            >
+              Add to Wishlist
+            </button> */}
         </div>
         <div className="w-1/3 p-2">
-          <button
-            onClick={addToWishlist}
-            className="text-gray-500 hover:text-gray-700"
-          >
-            Add to Wishlist
-          </button>
-        </div>
-        <div className="w-1/3 p-2">
-          <button onClick={shareProduct} className="text-gray-500 hover:text-gray-700">
-            Share
-          </button>
         </div>
         
       </div>
