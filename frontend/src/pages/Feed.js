c4c6ccb380b1e18bdeaf6c932307cc2ee825d258
junrongggg <junrongt4n@gmail.com>
2023-09-09 12:49:36 +0800

    added search bar in feed

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -27,10 +27,19 @@ export const Feed = () => {
   return (
     <div className="bg-white flex flex-col md:flex-row">
       {/* Left Section */}
-      <div className="w-full md:w-3/4">
-        <div className="mx-auto max-w-7xl py-20 px-20 sm:px-6 lg:px-8">
-          <div className="mt-10 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"></div>
-        </div>
+      <div class="w-full md:w-3/4 ">
+        <label class="relative block">
+          <span class="sr-only">Search</span>
+          <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+            <svg class="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"></svg>
+          </span>
+          <input
+            class="placeholder-italic placeholder-text-slate-400 block w-6/12 bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm mx-auto mt-8"
+            placeholder="Search for product"
+            type="text"
+            name="search"
+          />
+        </label>
       </div>
 
       {/* Right Section for Ads */}
